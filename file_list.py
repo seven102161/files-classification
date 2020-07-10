593509e4e64ceeae3bb6e76c532297f1e31ab7a5
seven102161 <66271241@qq.com>
2020-07-10 14:51:55 +0800

    add some notes

--- file_list.py
+++ file_list.py
@@ -10,8 +10,8 @@ def get_file_list(_dir, _file_list):
     new_dir = _dir
     if os.path.isfile(_dir):
         _file_list.append(_dir)
-        # # 若只是要返回文件文，使用这个
-        # file_list.append(os.path.basename(dir))
+        # # 若只是要返回文件名，使用这个
+        # _file_list.append(os.path.basename(dir))
     elif os.path.isdir(_dir):
         for s in os.listdir(_dir):
             # 如果需要忽略某些文件夹，使用以下代码
@@ -22,17 +22,40 @@ def get_file_list(_dir, _file_list):
     return _file_list
 
 
+def write_to_txt(name, dic):
+    name = name + '.txt'
+    with open(name, 'w') as f:
+        for k, v in dic.items():
+            f.write(f'{k}\n')
+            for i in v:
+                f.write(f'{i}\n')
+    print('File: {} saved!'.format(name))
+
+
+def write_to_json(name, dic):
+    name = name + '.json'
+    with open(name, 'w', encoding='utf-8') as fp:
+        json.dump(dic, fp)
+    print('File: {} saved!'.format(name))
+
+
 if __name__ == '__main__':
-    file_path = r'/Users/jiangzhiyi/synologydrive/python3'
+    file_path = r'E:\二级党委'  # 写入要扫描的文件夹
+    # 将扫描文件夹里的第一层目录做为KEY，并以此创建字典。
     file_keys = os.listdir(file_path)
     file_dict = dict()
     for key in file_keys:
         new_path = os.path.join(file_path, key)
         file_list = get_file_list(new_path, [])
+        # 去掉windows目录的盘符标识,Linux不需要。
+        file_list = [i.replace('D:', '') for i in file_list]  # C:,D:,E:,F:
         file_dict[key] = file_list
-    with open('file_list.json', 'w', encoding='utf-8') as fp:
-        json.dump(file_dict, fp)
-    print('写入成功')
+
+    # 将结果保存为txt文件
+    write_to_txt(r'E:\file_list', file_dict)
+
+    # 将结果保存存为json文件
+    write_to_json(r'E:\file_list', file_dict)
 
 
 
